2bccacc8648c484e4aefcd70e82ffb697e0568fe
Justice Martin <jujmart12@gmail.com>
2021-08-24 18:44:24 -0400

    Added project model

---
+++ app/models/project.py
@@ -0,0 +1,24 @@
+from .db import db
+from sqlalchemy.sql import func
+
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    title = db.Column(db.String(50), nullable=False, index=True)
+    description = db.Column(db.Text, nullable=False)
+    createdAt = db.Column(db.DateTime(timezone=True),
+                          nullable=False, server_default=func.now())
+    updatedAt = db.Column(db.DateTime(timezone=True),
+                          nullable=False, server_default=func.now(), onupdate=func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'title': self.title,
+            'description': self.description,
+            'createdAt': self.createdAt,
+        }
