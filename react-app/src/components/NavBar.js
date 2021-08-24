d1c5abe99c4251b0a7fa16f841a3d453724bb384
Justice Martin <jujmart12@gmail.com>
2021-08-24 16:27:59 -0400

    Removed userlist from navbar and a little bit of styling change for the navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,38 +1,22 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
-  );
-}
+	return (
+		<nav>
+			<Link to="/" exact={true}>
+				TechKnowBuild
+			</Link>
+			<NavLink to="/login" exact={true} activeClassName="active">
+				Login
+			</NavLink>
+			<NavLink to="/sign-up" exact={true} activeClassName="active">
+				Sign Up
+			</NavLink>
+			<LogoutButton />
+		</nav>
+	);
+};
 
 export default NavBar;
