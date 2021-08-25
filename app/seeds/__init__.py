68e612a0db9e27a89d264da443cd6f4524a6f25d
Justice Martin <jujmart12@gmail.com>
2021-08-25 12:26:31 -0400

    Added seeds for categories and projects

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .projects import seed_projects, undo_projects
+from .categories import seed_categories, undo_categories
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,6 +12,8 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_projects()
+    seed_categories()
     # Add other seed functions here
 
 
@@ -17,4 +21,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_categories()
     # Add other undo functions here
