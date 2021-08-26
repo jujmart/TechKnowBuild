aa1b71b2bd570892d0455a8bc92cdb29946e460d
Justice Martin <jujmart12@gmail.com>
2021-08-25 22:49:42 -0400

    Trying to fix https error on heroku

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -11,7 +11,7 @@ export function Home() {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		dispatch(getAllCategories());
+		// dispatch(getAllCategories());
 	}, [dispatch]);
 
 	useEffect(() => {
