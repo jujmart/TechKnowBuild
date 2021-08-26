6743743ffda9a60e5d614565a9851913de0cf012
Justice Martin <jujmart12@gmail.com>
2021-08-25 20:10:49 -0400

    Added logic to make sure that what is displayed on homepage is an image support and not a video support

--- react-app/src/components/ProjectCard.js
+++ react-app/src/components/ProjectCard.js
@@ -2,11 +2,30 @@ import { useSelector } from "react-redux";
 
 export function ProjectCard({ projectId }) {
 	const projects = useSelector((state) => state.projects);
+	const project_supports = useSelector((state) => state.project_supports);
+
+	const currentProject = projects[projectId];
+	let firstSupportId = null;
+	for (let i = 0; i < currentProject?.project_supportIds.length; i++) {
+		let currentProject_SupportId = currentProject.project_supportIds[i];
+		if (
+			project_supports[currentProject_SupportId]?.projectSupportType ===
+			"image"
+		) {
+			firstSupportId = currentProject_SupportId;
+			break;
+		}
+	}
 
 	return (
 		<div>
-			{/* <img src={projects[projectId]?.} /> */}
-			<div>{projects[projectId]?.title}</div>
+			{firstSupportId && (
+				<img
+					src={project_supports[firstSupportId]?.projectSupportUrl}
+					alt="Project Img"
+				/>
+			)}
+			<div>{currentProject?.title}</div>
 		</div>
 	);
 }
