6236297a2769f18d56197c2b985786d942ac4123
Justice Martin <jujmart12@gmail.com>
2021-09-02 17:00:15 -0400

    Added page not found page and added some styling for long descriptions for projects and instrcutions for steps

--- app/seeds/projects.py
+++ app/seeds/projects.py
@@ -9,9 +9,9 @@ tablet = Project(
 logic_probe = Project(userId=1, title="Universal Logic Probe",
                       description="""These days good quality oscilloscope is not an expensive instrument and , we can see it in most workbenches. With the advancement of oscilloscopes, simple testing tools such as logic probes are not as popular these days. However, if the oscilloscope or logic analyzer was out of reach, the logic probe is a handy instrument to check digital circuits.
 
-                      Also, in some cases, a logic probe is an easy option to check the functionality of low-speed logic circuits because it provides a real-time visual indication of the logic state without adjustments or calibrations.
+Also, in some cases, a logic probe is an easy option to check the functionality of low-speed logic circuits because it provides a real-time visual indication of the logic state without adjustments or calibrations.
 
-                      The logic probe design described in this article uses common and inexpensive ICs, including the popular NE555 timer and LM393 low voltage comparator IC. At the time of this writing, both ICs were prevalent in the market and cost less than US$ 0.25.""")
+The logic probe design described in this article uses common and inexpensive ICs, including the popular NE555 timer and LM393 low voltage comparator IC. At the time of this writing, both ICs were prevalent in the market and cost less than US$ 0.25.""")
 
 
 def seed_projects():
