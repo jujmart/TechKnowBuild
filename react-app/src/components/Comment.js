1c69cf0465412776779d0da6c55fe415f03dad87
Justice Martin <jujmart12@gmail.com>
2021-09-28 19:40:34 -0400

    Fixed updated time for comments

--- react-app/src/components/Comment.js
+++ react-app/src/components/Comment.js
@@ -5,6 +5,42 @@ import "./css/Comment.css";
 export default function Comment({ commentId, setCurrentCommentIds }) {
 	const comment = useSelector((state) => state.comments[commentId]);
 
+	function handleUpdatedTime() {
+		const t = comment.updatedAt.split(/[ :]/);
+		const monthObj = {
+			Jan: 0,
+			Feb: 1,
+			Mar: 2,
+			Apr: 3,
+			May: 4,
+			Jun: 5,
+			Jul: 6,
+			Aug: 7,
+			Sep: 8,
+			Oct: 9,
+			Nov: 10,
+			Dec: 11,
+		};
+		const updatedTime = new Date(
+			Date.UTC(t[3], monthObj[t[2]], t[1], t[4], t[5], t[6])
+		);
+		const currentTime = new Date();
+		const difference = currentTime - updatedTime;
+
+		if (difference < 60 * 1000) {
+			return "< 1m";
+		} else if (difference < 60 * 60 * 1000) {
+			return `${Math.floor(difference / (60 * 1000))}m`;
+		} else if (difference < 24 * 60 * 60 * 1000) {
+			return `${Math.floor(difference / (60 * 60 * 1000))}h`;
+		} else if (difference < 30 * 24 * 60 * 60 * 1000) {
+			return `${Math.floor(difference / (24 * 60 * 60 * 1000))}d`;
+		} else if (difference < 12 * 30 * 24 * 60 * 60 * 1000) {
+			return `${Math.floor(difference / (30 * 24 * 60 * 60 * 1000))}mo`;
+		}
+		return `${Math.floor(difference / (12 * 30 * 24 * 60 * 60 * 1000))}y`;
+	}
+
 	return (
 		<div className="comment_container">
 			<div className="comment_user-info">
@@ -14,7 +50,9 @@ export default function Comment({ commentId, setCurrentCommentIds }) {
 					className="comment_profile-photo"
 				/>
 				<div className="comment_username">{comment?.username}</div>
-				<div className="comment_updatedAt">{comment?.updatedAt}</div>
+				<div className="comment_updatedAt">
+					{comment ? handleUpdatedTime() : null}
+				</div>
 			</div>
 			<div className="comment_content">{comment?.content}</div>
 		</div>
